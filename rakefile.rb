18879b41220ac81b6d14e4f6bd4c7c966b0994a3
daleonpz <daleonpz@gmail.com>
2020-06-21 16:38:23 +0200

    update

--- rakefile.rb
+++ rakefile.rb
@@ -4,6 +4,7 @@ require 'rake/clean'
 ## Define Compiler/linker Variables 
 ##############################################
 
+
 SOURCE_DIR  = "src"
 INCLUDE_DIR = "include/"
 OBJ_DIR     = "build/obj"
